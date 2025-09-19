e9279dc0e2b04db07a2680a0d04ada1f5bb480ef
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-09-19 16:58:19 -0400

    problem 5 solved

--- projecteuler5.py
+++ projecteuler5.py
@@ -1,15 +1,21 @@
 def is_divisble(bound, num):
     test = 1
     while test < bound:
-        if (num % test != 0):
+        if num % test != 0:
             return False
         test = test + 1
     return True
 
 
 num = 1
-
-while num < 100000000:
+num_found = False
+while num < 10000000000 and num_found == False:
      if is_divisble(20,num):
-         print(num)
+        num_found = True
      num = num + 1
+
+
+print(num - 1)
+
+
+
