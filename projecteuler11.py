fb7fed081fcc042a5d61376dd20b911a61435201
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-09-24 19:45:05 -0400

    progress on problem 11

---
+++ projecteuler11.py
@@ -0,0 +1,22 @@
+with open("pe11", "r") as file:
+    lines = file.readlines()
+    nums = []
+    for line in lines:
+        num = line.split(" ")
+        for digit in num:
+            nums.append(int(digit))
+
+index = 0
+product = 0
+max = 0
+while index < len(nums):
+    product = nums[index] * nums[index + 21] * nums[index + 42] * nums[index + 63]
+    index += 1
+    if product > max:
+        max = product
+    print(max)
+
+
+
+print(nums[0])
+print(nums[21])
