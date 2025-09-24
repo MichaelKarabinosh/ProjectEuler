8bd9a66610472bbe16ac308d908cf27a7c2f47e3
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-09-24 18:50:43 -0400

    problem 9 more optimal

--- projecteuler9.py
+++ projecteuler9.py
@@ -1,3 +1,6 @@
+import math
+
+
 def check_triplet(a,b,c):
     if a**2 + b**2 == c**2:
         return True
@@ -9,25 +12,41 @@ c_bound = 0
 found = False
 sum_bound = 1000
 
-
 while a_bound < sum_bound and not found:
-    if check_triplet(a_bound, b_bound, c_bound) and a_bound + b_bound + c_bound == sum_bound:
-        found = True
-        break
     a_bound += 1
-    b_bound = a_bound
-    c_bound = c_bound
+    b_bound = 0
     while b_bound < sum_bound and not found:
-        if check_triplet(a_bound, b_bound, c_bound) and a_bound + b_bound + c_bound == sum_bound:
-            found = True
-            break
         b_bound += 1
-        c_bound = 0
-        while c_bound < sum_bound and not found:
-            if check_triplet(a_bound, b_bound, c_bound) and a_bound + b_bound + c_bound == sum_bound:
+        if ((sum_bound ** 2) - (2 * sum_bound * a_bound) - (2 * sum_bound * b_bound) + (2 * a_bound * b_bound)) == 0: # derived equation from setting c = sqrt(a^2 + b^2) equal to c = 1000 - a - b
+            c_bound = int(math.sqrt(a_bound ** 2 + b_bound ** 2))
+            if check_triplet(a_bound, b_bound, c_bound):
                 found = True
-                break
-            c_bound += 1
-
-print(a_bound, b_bound, c_bound, found)
+print("Found:",found)
+print(a_bound, b_bound, c_bound)
 print(a_bound * b_bound * c_bound)
+
+
+# while a_bound < sum_bound and not found:
+#     if check_triplet(a_bound, b_bound, c_bound) and a_bound + b_bound + c_bound == sum_bound:
+#         found = True
+#         break
+#     a_bound += 1
+#     b_bound = a_bound
+#     c_bound = c_bound
+#     while b_bound < sum_bound and not found:
+#         if check_triplet(a_bound, b_bound, c_bound) and a_bound + b_bound + c_bound == sum_bound:
+#             found = True
+#             break
+#         b_bound += 1
+#         c_bound = 0
+#         while c_bound < sum_bound and not found:
+#             if check_triplet(a_bound, b_bound, c_bound) and a_bound + b_bound + c_bound == sum_bound:
+#                 found = True
+#                 break
+#             c_bound += 1
+#
+# print(a_bound, b_bound, c_bound, found)
+# print(a_bound * b_bound * c_bound)
+#
+# a_bound = 0
+# b_bound = 0
