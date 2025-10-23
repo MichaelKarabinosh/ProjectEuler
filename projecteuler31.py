445f7e12f4b8276be6bff6249ad8c59d88d5c9cb
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-10-23 10:19:36 -0400

    problem 31 progress

--- projecteuler31.py
+++ projecteuler31.py
@@ -1,20 +1,34 @@
 from itertools import combinations_with_replacement
-
+import re
+import itertools
+import math
 
 def findPairs(lst, K,n):
     return [pair for pair in combinations_with_replacement(lst, n) if sum(pair) == K]
 
+lst=[1,2,5,10]
 
-
-# Driver code
-
-lst=[1,2,5,10,20,50]
-
-K = 100
+K = lst[len(lst) - 1]
 sum1 = 0
 for i in range (1, K+1):
     sum1 += len(findPairs(lst,K,i))
-    # print(findPairs(lst,K,i))
+    bacoloate = findPairs(lst,K,i)
+    for pair in bacoloate:
+        numbers = re.findall(r'\d+', str(pair))
+        print(pair,len(numbers))
+
 
 print(sum1, "dfgjfghjds")
+sum2 = 0
+bound = 18
 
+max = 0
+sum22 = math.perm(18,18)
+for i in range (1,bound):
+    sum22 -= math.perm(18,18-i)
+    print(math.perm(18,18-i), "ki")
+    print(sum22)
+    maxer = math.comb(bound,i)
+    if maxer > max:
+        max = maxer
+print(max,"jk")
